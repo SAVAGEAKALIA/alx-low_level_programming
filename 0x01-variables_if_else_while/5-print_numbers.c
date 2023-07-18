89ba028413a6eb1c8a44f62a3f8a92a61d38e4b9
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-07-18 15:29:31 +0100

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - numbers
+*
+* Return: return = 0 (success)
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	while(i <= 9)
+{
+	printf("%d", i);
+	i++;
+}
+	putchar('\n');
+	return (0);
+}
