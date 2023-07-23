93ab0719d5df918982465c757867c552805aaf57
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-07-23 12:03:49 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - check the code.
+*
+* Return: return = 0 (success)
+*/
+
+void print_alphabet_x10(void)
+{
+	char i;
+	int x;
+
+	for (x = 0; x < 9; x++)
+{
+	for (i = 'a'; i <= 'z'; i++)
+{
+	_putchar(i);
+}
+	_putchar('\n');
+}
+}
