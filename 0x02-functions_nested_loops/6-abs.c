94f4a1d76b0c1d4037ae1059f064042fdc85672a
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-07-23 14:12:30 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - check the code
+ * @a: Absolute
+ * Return: Always 0.
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+	return (-a);
+	else if (a > 0)
+{
+	return (a);
+}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+
+int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
