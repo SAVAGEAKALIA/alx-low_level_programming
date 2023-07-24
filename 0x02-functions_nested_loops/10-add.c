410cc7a33d5b90c69991e2b4fc588a70b2f0834e
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-07-24 10:17:11 +0100

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - check the code
+ *@i: addition
+ *@j: addition
+ * Return: return: i + j (success)
+ */
+
+int add(int i, int j)
+{
+
+	return (i + j);
+}
