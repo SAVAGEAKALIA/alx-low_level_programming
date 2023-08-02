c3818b3616f2826ce27a312dcf7492390b49f757
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-02 12:40:09 +0100

    functions and nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,13 +19,15 @@ void print_diagonal(int n)
 }
 	else
 {
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 {
-	for (j = 1; j <= i; j++)
+	for (j = 0; j < n; j++)
 		{
+		if (j == i)
+		_putchar('\\');
+		else if (j < i)
 		_putchar(' ');
 		}
-		_putchar('\\');
 		_putchar('\n');
 }
 }
