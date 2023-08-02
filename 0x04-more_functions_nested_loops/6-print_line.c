8cf48a7c8c14ec7439162ceea6d3f6c73fc19033
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-02 12:06:51 +0100

    functions and nested loops

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,12 +14,15 @@ void print_line(int n)
 
 	int i;
 
+	if (n <= 0)
+	_putchar('\n);
+
+	else
+{
 	for (i = 0; i <= n; i++)
 	{
 		_putchar('_');
 	}
-	if (n <= 0)
-	_putchar('\n');
-	else
-	_putchar('\n');
+		_putchar('\n');
+}
 }
