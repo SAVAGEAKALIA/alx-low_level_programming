b841bfde09bbeff0c7aa476bd9309d775c26df49
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-02 15:26:43 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+*_strlen - return string number
+*
+*@s: data type string
+*Return: return strlen lenght
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
