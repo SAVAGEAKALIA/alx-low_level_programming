b4ba580176bdffc2f4368f524ec6dd09b9837122
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-03 05:31:43 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*print_rev - imprime en reversa
+*@s: string
+*
+*Return: 0
+*/
+
+void print_rev(char *s)
+{
+	int begin, end = 0;
+
+	while (*s != '\0')
+{
+	end++;
+	s++;
+}
+	s = s - 1;
+
+	for (begin = end; begin >= 0; begin--)
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}
