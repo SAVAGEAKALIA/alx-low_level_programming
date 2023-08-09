6fdaa749e7609065245113d55b6662ea1b9c606f
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-09 06:16:20 +0100

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,19 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i;
 	int s1_length = strlen(s1);
 	int s2_length = strlen(s2);
-	int size = ((s1_length + s2_length) + 1);
+	int size = (s1_length + s2_length + 1);
 	char *s = malloc(sizeof(char) * size);
 
 	if (s == NULL)
 	{
 	return (NULL);
 	}
-	for (i = 0; i < s1_length; i++)
-{
-	s[i] = s1[i];
-}
-	for (i = 0; i < s2_length; i++)
-{
-	s[s1_length + i] = s2[i];
-}
 
-	s[s1_length + s2_length] = '\0';
+
+	strcpy(s, s1);
+	strcat(s, s2);
 
 	return (s);
 }
