2adeb1e14f1672eaa5cb2a376f8f7c40b18f36aa
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-10 04:52:15 +0100

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -4,36 +4,53 @@
 #include <string.h>
 
 /**
+*alloc_grid - function to print 2d array
+*@width: column of array
+*@height: row of array
 *
-*
-*
-*
-*
+*Return: grid on success
 */
 
 int **alloc_grid(int width, int height)
 {
-	int i,j;
+	int i, j;
+	int **grid;
 
-	int size = (width + height);
-	char *s = malloc(sizeof(int) * size);
+/*Check for if width and height is 0 or negative*/
+	if (width <= 0 || height <= 0)
+	return (NULL);
 
-	if (s == NULL)
+/* allocate memmory for each row for pointer to pointer of arrays of integer*/
+	grid = (int **)malloc(sizeof(int *) * height);
+
+/*Check if grid is NULLand terminate Code by returning NULL*/
+
+	if (grid == 0)
+	return (NULL);
+
+	for (i = 0; i < height; i++)
 {
+	grid[i] = malloc(sizeof(int) * width);
+
+/* Check if grid[i] is equal to zero(0) */
+
+	if (grid[i] == 0)
+{
+/* Frees memmory for gri and grid[i] before returning NULL */
+
+	for (j = 0; j < i; j++)
+	{
+		free(grid[j]);
+	}
+	free(grid);
 	return (NULL);
 }
-
-	if (width <=0 || height <= 0)
-{
-	return (NULL);
 }
-
-	for (i = 0; i < size; i++)
+	for (i = 0; i < height; i++)
 {
-		for (j = 0; j < size; j++)
+	for (j = 0; j < width; j++)
 {
-		_putchar(s[0]);
+	grid[i][j] = 0;
 }
-}
-		return (0);
+}	return (grid);
 }
