872bc4496263fface3d9f89b5ab687e281144481
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-12 08:36:51 +0100

    0-malloc_checked.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 /**
 *string_nconcat - function to concatenate strings
 *@s1: Data type
@@ -25,7 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	while (s2 && s2[s2_lenght])
 	s2_lenght++;
 
-	result = malloc(sizeof(char) * (s1_lenght + ((n >= s2_lenght) ? s2_lenght : n) + 1));
+	result = malloc(sizeof(char) * (s1_lenght + ((n >= s2_lenght) ? s2_lenght :
+					n) + 1));
 
 	if (result == NULL)
 	{
@@ -37,8 +37,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	result[i] = s1[i];
 	}
 
-	for (i = s1_lenght; (n < s2_lenght || n >= s2_lenght) && i < ((n >= s2_lenght) ? s2_lenght : n) + s1_lenght; i++)
+	for (i = s1_lenght; (n < s2_lenght || n >= s2_lenght) && i < ((n >=
+					s2_lenght) ? s2_lenght : n) + s1_lenght; i++)
 	result[i] = s2[j++];
 
 	result[i] = '\0';
-	return (result); }
+	return (result);
+}
