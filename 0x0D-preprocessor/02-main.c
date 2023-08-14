3464d65c5183e6c153c37ab150b2ce8c2e2911e5
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-14 19:49:47 +0100

    2-main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+*main - function to print name of file
+*
+*Function to print same file name
+*Return: Always 0 on (success)
+*/
+
+
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
