46e382563ffc8f9cab1c0d27dc2adb6ae05ce467
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-15 04:27:09 +0100

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - structure dog task
+  *@name: first member
+  *@age: second member
+  *@owner: third member
+  *Description: Structure function with three arguments
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
