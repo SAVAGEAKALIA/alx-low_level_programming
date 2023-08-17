8248290ae214033fe0502c3598a611468f2b41bf
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-17 05:53:19 +0100

    0x0F. C - Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,20 +12,9 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *n;
 	size_t i;
-
-	n = malloc(sizeof(int) * size);
-
-	if (n == NULL)
-		{return;
-		}
-
 			for (i = 0; i < size; i++)
 			{
-			n[i] = array[i];
-			action(n[i]);
+			action(array[i]);
 			}
-
-			free(n);
 }
