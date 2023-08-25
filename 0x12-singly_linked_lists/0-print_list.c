9a873cdfa7310535205c6ac32eb921bde5ec94e5
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-25 05:54:04 +0100

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+  *print_list - singly list array
+  *@h: pointer to structure
+  *Return: n on success
+  */
+
+size_t print_list(const list_t *h)
+{
+	size_t n = 0;
+	const list_t *string;
+
+	 string = h;
+
+		while (string != NULL) /*created a loop for each node*/
+	{
+			if (string->str == NULL)
+			{
+				printf("[0] (nil)");
+			}
+			else
+			{
+			printf("[%d] %s", string->len, string->str);
+			}
+
+			string = string->next;/*moves to the next node*/
+			n++;
+
+		printf("\n");
+	}
+	return (n);
+}
