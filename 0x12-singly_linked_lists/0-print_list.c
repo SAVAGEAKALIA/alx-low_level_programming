1fbbe0180ba39c5fc25947589a0eb872547bb66e
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-26 07:52:34 +0100

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,11 +12,15 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[0] (nil)");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+		{
+			printf("[%u] %s", h->len, h->str);
+		}
 		h = h->next;
 		n++;
+
+		printf("\n");
 	}
 	return (n);
 }
