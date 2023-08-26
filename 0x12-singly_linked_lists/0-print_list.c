9c614428ef0c24de7821a7f449c2b714110bae6d
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-26 07:20:44 +0100

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 				printf("[0] (nil)\n");
 			}
 			else
-			
+
 			printf("[%u] %s\n", string->len, string->str);
 			string = string->next;/*moves to the next node*/
 			n++;
