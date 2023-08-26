732e7756a982d95148a4d813f3920fe6cfb04e79
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-26 07:50:30 +0100

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,16 +9,14 @@ size_t print_list(const list_t *h)
 {
 	size_t n = 0;
 
-		while (h != NULL) /*created a loop for each node*/
+	while (h != NULL)
 	{
-			if (h->str == NULL)
-				printf("[0] (nil)\n");
-
-			else
-				printf("[%u] %s\n", h->len, h->str);
-			h = h->next;/*moves to the next node*/
-			n++;
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		n++;
 	}
-
 	return (n);
 }
