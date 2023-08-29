005b723bd517e394df2e1bf967f97858383d3fd2
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-29 10:40:58 +0100

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+  *free_listint2 - free linked list
+  *@head: pointer to list
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp = *head;
+
+	while (temp != NULL)
+	{
+		listint_t *freenode = temp;
+
+		temp = temp->next;
+		free(freenode);
+	}
+	*head = NULL;
+}
