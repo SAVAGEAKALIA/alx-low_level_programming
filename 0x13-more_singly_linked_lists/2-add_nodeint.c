ac4a742a4c6e49caf3e620d5fe4e525b3280ca73
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-29 06:04:43 +0100

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -8,18 +8,19 @@
 
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *add_node = malloc(sizeof(listint_t));
+	listint_t *add_node;
 
+	add_node = malloc(sizeof(listint_t));
 	if (add_node == NULL)
-		return (NULL);
-
-
-	while (add_node != NULL)
 	{
+		return (NULL);
+	}
+
+
 		add_node->n = n;
 		add_node->next = *head;
 
 		*head = add_node;
-	}
+
 	return (add_node);
 }
