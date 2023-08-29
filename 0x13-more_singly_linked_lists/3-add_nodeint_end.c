4bb339701720bdfa9aa7ddd786a5af842ab4ebac
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-08-29 09:37:15 +0100

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+  *
+  *
+  *
+  */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *end_node;
+
+	listint_t *last = *head;
+
+		end_node = malloc(sizeof(listint_t));
+	if (end_node == NULL)
+	{
+		return (NULL);
+	}
+	
+	end_node->n = n;
+	end_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = end_node;
+	}
+	else
+	{
+		while (last->next != NULL)
+		{
+			last = last->next;
+
+		}
+		last->next = end_node;
+	}
+
+		return (end_node);
+}
