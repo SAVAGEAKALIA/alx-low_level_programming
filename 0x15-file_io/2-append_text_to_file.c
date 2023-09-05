982a1f369e540545f7101e72540da2afb3982a96
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-09-05 05:45:52 +0100

    0x15. C - File I/O

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,8 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (size = 0; text_content[size];)
-			size++;
+		size = strlen(text_content);
 	}
 
 	writeonly = open(filename, O_WRONLY | O_APPEND);
