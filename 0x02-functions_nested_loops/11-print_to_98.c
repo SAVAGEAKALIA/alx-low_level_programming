3d641fda40c9011ad2201c0f1f78a459a745e0db
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-10-26 14:00:35 +0100

    update

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,9 +14,12 @@ void print_to_98(int n)
 	if (n >= 0 && n <= 98)
 {
 	for (i = n; i <= 98; i++)
+	{
 	printf("%d", i);
 	if (i != 98)
 	printf(", ");
+	}
+	printf("\n");
 }
 
 	else if (n < 0)
@@ -31,7 +34,7 @@ void print_to_98(int n)
 }
 	else if (n > 98)
 {
-	for (i = n; i >= 98; i++)
+	for (i = n; i >= 98; i--)
 {
 	printf("%d", i);
 	if (i != 98)
@@ -39,8 +42,6 @@ void print_to_98(int n)
 }
 	printf("\n");
 }
-	else if (n == 98)
+	else
 	printf("%d\n", n);
 }
-
-
