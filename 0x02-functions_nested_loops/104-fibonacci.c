ba5f02944bd24a1c69d403f9c77cbd62899dc569
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-10-27 10:03:26 +0100

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-	unsigned long long nth_term = 0;
-	unsigned long long t1 = 1, t2 = 2;
-	unsigned long long n;
+	unsigned long nth_term = 0;
+	unsigned long t1 = 1, t2 = 2;
+	unsigned long n;
 
 
-	printf("%llu, %llu", t1, t2);
+	printf("%lu, %lu", t1, t2);
 
 	for (n = 2; n < 98; n++)
 	{
 		nth_term = t1 + t2;
-		printf(", %llu", nth_term);
+		printf(", %lu", nth_term);
 		t1 = t2;
 		t2 = nth_term;
 	}
