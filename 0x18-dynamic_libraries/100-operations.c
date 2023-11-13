24f60029ef9537a4cb0044346acd8fbb39585717
SAVAGEAKALIA <akaliasaviour@gmail.com>
2023-11-14 00:08:34 +0100

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+/**
+ *add - returns the remainder
+ *@a: variable a
+ *@b: variable b
+ *Return: add on success
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - returns the remainder
+ *@a: variable a
+ *@b: variable b
+ *Return: sub on success
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *mul - returns the remainder
+ *@a: variable a
+ *@b: variable b
+ *Return: mul on success
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *div - returns the remainder
+ *@a: variable a
+ *@b: variable b
+ *Return: div on success
+ */
+int  div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ *mod - returns the remainder
+ *@a: variable a
+ *@b: variable b
+ *Return: mod on success
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
